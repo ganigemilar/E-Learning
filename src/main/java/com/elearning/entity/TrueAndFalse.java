68b26afc47fd5c166f6f75019cff0472cb7435f4
ganigemilar <ganigemilar10@gmail.com>
2019-05-07 15:50:51 +0700

    update exam

--- src/main/java/com/elearning/entity/TrueAndFalse.java
+++ src/main/java/com/elearning/entity/TrueAndFalse.java
@@ -1,7 +1,26 @@
 package com.elearning.entity;
 
 public class TrueAndFalse extends Exam {
+    private boolean isTrueChoiced;
+    private boolean isFalseChoiced;
+
     public TrueAndFalse(String question) {
         super(question);
+    }
+
+    public boolean isTrueChoiced() {
+        return isTrueChoiced;
+    }
+
+    public void setTrueChoiced(boolean trueChoiced) {
+        isTrueChoiced = trueChoiced;
+    }
+
+    public boolean isFalseChoiced() {
+        return isFalseChoiced;
+    }
+
+    public void setFalseChoiced(boolean falseChoiced) {
+        isFalseChoiced = falseChoiced;
     }
 }
